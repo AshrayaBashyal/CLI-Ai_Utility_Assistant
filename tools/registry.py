778065f6ba6edb860cae00ea0491af25199f1c19
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-01 14:54:58 +0545

    add convert tool in tool_registry

--- tools/registry.py
+++ tools/registry.py
@@ -1,8 +1,10 @@
 from tools.calculator import calculator
 from tools.time import current_time
+from tools.converter import convert
 
 
 TOOL_REGISTRY = {
     "calculator": calculator,
     "current_time" : current_time,
+    "convert" : convert
 }

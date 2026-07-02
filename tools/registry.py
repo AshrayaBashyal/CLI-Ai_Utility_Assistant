aa39a21f42143374fbea2eb8d92a146715c05725
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-02 20:51:17 +0545

    register currency converter tool in tool registry

--- tools/registry.py
+++ tools/registry.py
@@ -2,11 +2,13 @@ from tools.calculator import calculator
 from tools.time import current_time
 from tools.converter import convert
 from tools.weather import get_weather
+from tools.currency_convert import convert_currency
 
 
 TOOL_REGISTRY = {
     "calculator": calculator,
     "current_time" : current_time,
     "convert" : convert,
-    "get_weather" : get_weather
+    "get_weather" : get_weather,
+    "convert_currency" : convert_currency
 }

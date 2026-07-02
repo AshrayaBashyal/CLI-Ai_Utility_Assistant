c4f138046cd200ed37cb4906e0fe1b3257b5ab09
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-02 20:50:02 +0545

    add currency converter tool and rename converter tool to unit converter tool

--- schemas/tools.py
+++ schemas/tools.py
@@ -40,7 +40,7 @@ CURRENT_TIME_TOOL = {
 }
 
 
-CONVERTER_TOOL = {
+UNIT_CONVERTER_TOOL = {
     "type": "function",
     "function": {
         "name": "convert",
@@ -107,11 +107,39 @@ WEATHER_TOOL = {
     },
 }
 
+CURRENCY_CONVERTER_TOOL = {
+    "type": "function",
+    "function": {
+        "name": "convert_currency",
+        "description": "Converts currency amounts between dynamic fiat options using live central bank indices.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "amount": {
+                    "type": "number",
+                    "description": "The exact total numerical value of money to convert. Must be greater than 0."
+                },
+                "from_currency": {
+                    "type": "string",
+                    "description": "The standard 3-letter currency code to convert from (e.g., USD, EUR, GBP)."
+                },
+                "to_currency": {
+                    "type": "string",
+                    "description": "The standard 3-letter currency code to convert into (e.g., NPR, JPY, CAD)."
+                }
+            },
+            "required": ["amount", "from_currency", "to_currency"]
+        }
+    }
+}
+
+
 
 
 TOOLS = [
     CALCULATOR_TOOL,
     CURRENT_TIME_TOOL,
-    CONVERTER_TOOL,
-    WEATHER_TOOL
+    UNIT_CONVERTER_TOOL,
+    WEATHER_TOOL,
+    CURRENCY_CONVERTER_TOOL
 ]

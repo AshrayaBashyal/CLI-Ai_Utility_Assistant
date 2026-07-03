fc60cc844f750f0d314a0b92bb53b5a06bb846f5
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-03 20:29:22 +0545

    fix import error using relative import

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,10 +1,10 @@
-from calculator import CALCULATOR_TOOL
-from weather import WEATHER_TOOL
-from currency import CURRENCY_CONVERTER_TOOL
-from converter import UNIT_CONVERTER_TOOL
-from current_time import CURRENT_TIME_TOOL
-from send_email import EMAIL_TOOL
-from contacts import CONTACTS_TOOL
+from .calculator import CALCULATOR_TOOL
+from .weather import WEATHER_TOOL
+from .currency import CURRENCY_CONVERTER_TOOL
+from .converter import UNIT_CONVERTER_TOOL
+from .current_time import CURRENT_TIME_TOOL
+from .send_email import EMAIL_TOOL
+from .contacts import CONTACTS_TOOL
 
 
 TOOLS = [

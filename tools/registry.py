062b12e78266c32178f6279b1cd026473c6b81e0
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-07-03 11:57:16 +0545

    regster get_contact tool

--- tools/registry.py
+++ tools/registry.py
@@ -3,6 +3,7 @@ from tools.time import current_time
 from tools.converter import convert
 from tools.weather import get_weather
 from tools.currency_convert import convert_currency
+from tools.contacts import get_contact
 
 
 TOOL_REGISTRY = {
@@ -10,5 +11,6 @@ TOOL_REGISTRY = {
     "current_time" : current_time,
     "convert" : convert,
     "get_weather" : get_weather,
-    "convert_currency" : convert_currency
+    "convert_currency" : convert_currency,
+    "get_contact" : get_contact
 }
